e7060f1a96263b54f800d1293be86b4e008e0834
Camilo Mazo <camilo.mazo2@udea.edu.co>
2021-04-19 14:12:12 -0500

    Menu del usuario listo

--- codigoFuente/main.cpp
+++ codigoFuente/main.cpp
@@ -3,10 +3,11 @@
 using namespace std;
 
 void verificacion();
+void menu();
 
 int main()
 {
-     verificacion();
+    menu();
     return 0;
 }
 
@@ -18,4 +19,28 @@ void verificacion(){
      for(int i=0;i<8;i++){
        cout<<"Acá se apagan los leds para usarlos nuevamente con otra función"<<endl;
      }
+}
+
+void menu(){
+    int spacesl=21;
+    int spacesr=0;
+    int ans=0;
+    for(int i=0;i<8;i++){
+        cout<<"[8][8][8][8][8][8][8][8]"<<endl;
+        cout<<"[7][7][7][7][7][7][7][7]"<<endl;
+        cout<<"[6][6][6][6][6][6][6][6]"<<endl;
+        cout<<"[5][5][5][5][5][5][5][5]"<<endl;
+        cout<<"[4][4][4][4][4][4][4][4]"<<endl;
+        cout<<"[3][3][3][3][3][3][3][3]"<<endl;
+        cout<<"[2][2][2][2][2][2][2][2]"<<endl;
+        cout<<"[1][1][1][1][1][1][1][1]"<<endl;
+       for(int j=0;j<spacesl;j++) cout<<" ";
+       cout<<"^^^";
+       for(int j=0;j<spacesr;j++) cout<<" ";
+       spacesl-=3;
+       spacesr+=3;
+       cout<<endl<<"ingrese"<<endl;
+       cin>>ans;
+    }
+
 }
